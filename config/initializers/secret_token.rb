7eec207f4fce154eb66f561439dbe053201cee41
App Academy Student <clementpigeon@gmail.com>
2013-09-19 16:57:38 -0400

    creates project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoApp::Application.config.secret_token = '669444bf2a23a7555d3215caf7f63ca758182a646382c12e3f8fa59ecc9a25c7d91f2151516bb5f63777976c57c655bd02e41fe1ed5344c627c1c64649664ace'
