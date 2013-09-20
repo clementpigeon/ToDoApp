3d90589ebbfebab74b9db7c5cadcceb0d1f85bb3
App Academy Student <appacademy@Hoofddorp-5.local>
2013-09-20 10:37:27 -0400

    fixes project controller and create form

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name, :team_id
 
-  validates :name, presence: true, uniqueness: { scope: :nameteam_id }
+  validates :name, presence: true, uniqueness: { scope: :team_id }
 
   has_many :items
 
